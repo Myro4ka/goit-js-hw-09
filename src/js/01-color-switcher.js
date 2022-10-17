7b9b6c82faa783d97c4a027320f49f99cdbc3015
Myro4ka <www.mirosya@gmail.com>
2022-10-18 01:06:42 +0200

    Change bgColor in 01-color-switcher.js

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,37 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let timerId = 0;
+
+if (timerId === 0) {
+  stopBtn.setAttribute('disabled', 'disabled');
+}
+
+startBtn.addEventListener('click', onStartBtn)
+stopBtn.addEventListener('click', onStopBtn)
+
+function onStartBtn() {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor()
+  }, 1000);
+  toggleStateEl(startBtn, stopBtn, 'disabled');
+}
+
+function onStopBtn() {
+  clearInterval(timerId);
+  toggleStateEl(startBtn, stopBtn, 'disabled');
+}
+
+function toggleStateEl(el1, el2, atribute) {
+  if (!el1.hasAttribute(atribute)) {
+    el1.setAttribute(atribute, atribute);
+    el2.removeAttribute(atribute);
+  } else {
+    el1.removeAttribute(atribute);
+    el2.setAttribute(atribute, atribute);
+  }
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
